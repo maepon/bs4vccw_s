774157cd834cf5a08ab31f45af49eb727536f8b9
Maekawa <maekawa@iMackun-no-iMac.local>
2014-10-15 15:17:17 +0900

    commit

---
+++ README.md
@@ -0,0 +1,39 @@
+## Browser-Sync for VCCW Underscores Theme
+
+Browser-Sync Setting for VCCW with compiling Underscores Theme's sass.
+
+[VCCW](http://vccw.cc/) is Vagrant configuration designed for development of WordPress plugins, themes, or websites.
+
+based on [gaspanik/bs4vccw](https://github.com/gaspanik/bs4vccw)
+
+## Usage
+
+### Clone to your VCCW directory. 
+
+	git clone https://github.com/maepon/bs4vccw_s.git
+		
+### Edit your vhost name and theme path in 'gulpfile.js'.
+
+> var paths = {  
+>   "vhost": "wordpress.local",  
+>   "themes": "../www/wordpress/wp-content/themes/_s/**"  
+>  "sassDir": "../www/wordpress/wp-content/themes/_s/**/*.scss",
+>  "sass": "../www/wordpress/wp-content/themes/_s/sass/style.scss",
+>  "style": "../www/wordpress/wp-content/themes/_s/"
+> }
+
+
+### Install some dependencies.
+
+	npm install
+
+### Run gulp.
+
+	npm start
+
+### Open browser
+
+Connect to 'http://localhost:3000'
+
+---
+MIT
---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp'),
+  browserSync = require('browser-sync'),
+  $ = require('gulp-load-plugins')();
+
+var paths = {
+  "vhost": "wordpress.local",
+  "themes": "../www/wordpress/wp-content/themes/_s/**",
+  "sassDir": "../www/wordpress/wp-content/themes/_s/**/*.scss",
+  "sass": "../www/wordpress/wp-content/themes/_s/sass/style.scss",
+  "style": "../www/wordpress/wp-content/themes/_s/"
+}
+
+gulp.task('bs-init', function() {
+  browserSync.init(null, {
+    proxy: paths.vhost,
+    notify: true
+  });
+});
+
+gulp.task('bs-reload', function() {
+  browserSync.reload()
+});
+
+gulp.task('styles', function() {
+  return gulp.src([paths.sass])
+    // .pipe($.changed(paths.style, {extension: '.css'}))
+    .pipe($.rubySass({
+      style: 'nested',
+      precision: 10
+    }))
+    .pipe(gulp.dest(paths.style))
+});
+
+gulp.task('watch', function() {
+  gulp.watch([paths.sassDir], ['styles','bs-reload']);
+  gulp.watch([paths.themes], ['bs-reload']);
+});
+
+gulp.task('default', ['bs-init', 'bs-reload', 'watch']);
